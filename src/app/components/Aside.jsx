7cbd52d6ce678ddf5aea4412766bdd4040015d02
Paulo Victor Belens <paulovictor55296@gmail.com>
2023-07-24 16:25:54 -0300

    executando as mudanças no Aside

--- src/app/components/Aside.jsx
+++ src/app/components/Aside.jsx
@@ -2,13 +2,20 @@ import styles from '../page.module.css'
 import Image from 'next/image'
 import logoLight from '../../images/logo-light.svg'
 import logoDark from '../../images/logo-dark.svg'
+import AsideBoards from './AsideBoards'
 
 export default function Aside() {
   return (
-    <aside>
+    <aside className={styles.asideContainer}>
         <div className={styles.logo}>
           <Image src={logoDark} alt="Kanban Logo" width={200} height={50} priority />
         </div>
+        <div className={styles.asideContent}> 
+          <div>
+            <h3 className={styles.asideTitle}>ALL BOARDS (3)</h3>
+          </div>
+        <AsideBoards />
+      </div>
     </aside>
   )
 }
---
+++ src/app/components/AsideBoards.jsx
@@ -0,0 +1,20 @@
+import styles from '../page.module.css';
+
+const AsideBoards = () => {
+  return (
+  <>
+    <div className={styles.asideBoards}>
+    <h4>(symbol) Platform</h4>
+    <h4>(symbol) Markenting Plan</h4>
+    <h4>(symbol) Roadmap</h4>
+  </div>
+  <div>
+    <h4>
+      +Create New Board
+    </h4>
+  </div>
+  </>
+  )
+};
+
+export default AsideBoards;
