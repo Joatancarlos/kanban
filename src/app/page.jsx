7ef81ee96904bb75206e7dc9ef138365b3311469
Joatan Feitosa <joatancarlos@gmail.com>
2023-07-24 15:50:38 -0300

    estilizando o css do header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.layout}>
       <Aside />
-      <div>
+      <div className={styles.layoutMain}>
         <Header />
         <Main />
       </div>
