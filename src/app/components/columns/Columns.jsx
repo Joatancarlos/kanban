df0be2f28ac9d2aa569c83fd7e2d9ed13f959cef
Paulo Victor Belens <paulovictor55296@gmail.com>
2023-07-28 16:38:36 -0300

    ajustando a identação

--- src/app/components/columns/Columns.jsx
+++ src/app/components/columns/Columns.jsx
@@ -7,25 +7,26 @@ const Columns = ({columns}) => {
 
         <h3>{`${name} (${tasks ? tasks.length : "0"})`}</h3>
 
-          {tasks && tasks.map(({id, title, description, status, subtasks}) => (
+        {tasks && tasks.map(({id, title, description, status, subtasks}) => (
           <>
-          <div key={id}>
-            <h2>{title}</h2>
-            <h2>{description}</h2>
-            <h2>{status}</h2>
-          </div>
-          <div>
-            {subtasks && 
-              subtasks.map(({id, title}) => (
-                <div key={id}>
-                  <h2>{title}</h2>
-                </div>
-              ))}
-          </div>
+            <div key={id}>
+              <h2>{title}</h2>
+              <h2>{description}</h2>
+              <h2>{status}</h2>
+            </div>
+            <div>
+              {subtasks && 
+                subtasks.map(({id, title}) => (
+                  <div key={id}>
+                    <h2>{title}</h2>
+                  </div>
+                ))}
+            </div>
           </>
         ))}
 
       </div>
+
     ))}
 
     </>
