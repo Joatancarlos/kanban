c9e6c8866dd04a48985f67a7a522bde0b905dfd0
Joatan Feitosa <joatancarlos@gmail.com>
2023-07-31 21:01:08 -0300

    css do modal delete feito

--- src/app/components/modals/ModalDelete.jsx
+++ src/app/components/modals/ModalDelete.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Modal from 'react-modal';
+import styles from '../../page.module.css'
 
 export default function ModalDelete({boardOrTask , modalDeleteBoard, showDeleteBox, deleteFunction, children}) {
   const customStyles = {
     content: {
-      width: '25vw', 
       top: '50%',
       left: '50%',
       right: 'auto',
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
+      transition: 'all 0.4s ease-in-out',
     },
   };
   return (
@@ -22,15 +23,17 @@ export default function ModalDelete({boardOrTask , modalDeleteBoard, showDeleteB
         style={customStyles}
         ariaHideApp={false}
       >
-        <div>
-          <h3>Delete this {boardOrTask}?</h3>
-          <p>{children}</p>
-          <button type="button" onClick={deleteFunction}>
-            Delete
-          </button>
-          <button type="button" onClick={() => showDeleteBox()}>
-            Cancel
-          </button>
+        <div className={styles.modalDivDelete}>
+          <h3 className={styles.titleDelete}>Delete this {boardOrTask}?</h3>
+          <p className={styles.txtDelete}>{children}</p>
+          <div className={styles.buttonsDelete}>
+            <button className={styles.btn} type="button" onClick={deleteFunction}>
+              Delete
+            </button>
+            <button className={styles.btn} type="button" onClick={() => showDeleteBox()}>
+              Cancel
+            </button>
+          </div>
         </div>
       </Modal>
   )
